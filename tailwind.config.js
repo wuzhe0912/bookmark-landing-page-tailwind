75e9419e7af02441b9b756395d5c95bebf832d12
Pitt Wu <kgb00128@gmail.com>
2021-11-29 16:39:52 +0800

    feat: setting tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"]
+      },
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
